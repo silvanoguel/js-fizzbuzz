2e78ff0bee45bee989a14bd599566496dd4b820d
silvanoguel <silvanoguel@msn.com>
2023-03-10 14:26:11 +0100

    add:milestone 2 and style to DOM

--- js/script.js
+++ js/script.js
@@ -1,10 +1,40 @@
+// MILESTONE 1
 // % 3 = multipli di 3
 // % 5 = multipli di 5
 // % 15 = (3x5) multipli di 15 (più specifico)
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 15 === 0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log (i);
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 15 === 0) console.log("FizzBuzz");
+//     else if (i % 3 === 0) console.log("Fizz");
+//     else if (i % 5 === 0) console.log("Buzz");
+//     else console.log (i);
+// }
+
+
+// MILESTONE 2
+const gridElem = document.querySelector(".grid");
+for (let i = 1; i <= 100; i++) {
+    let fizzBuzz;
+    let classColor;
+    if (i % 15 === 0) {
+        fizzBuzz = "fizzBuzz";
+        classColor = "fizzbuzz";
+    }   else if (i % 3 === 0) {
+        fizzBuzz = "fizz";
+        classColor = "fizz";
+    }   else if (i % 5 === 0) {
+        fizzBuzz = "buzz";
+        classColor = "buzz";
+    }  else {
+        fizzBuzz = "buzz";
+        classColor = "other";
+    }
+    console.log(i, fizzBuzz);
+    gridElem.innerHTML += `<div class="box ${classColor}">${i}</div>`;
 }
+
+
+
+
+
+
